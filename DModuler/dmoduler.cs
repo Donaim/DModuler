52eb496371fd2b8a6f2659b61095833310391f7e
d0naim <d0naim@jerrs.dsnet.umcs.pl>
2018-01-28 13:08:47 +0100

    Reworked DModuler to new style of OutputEx

--- DModuler/dmoduler.cs
+++ DModuler/dmoduler.cs
@@ -19,37 +19,37 @@ namespace DModulerSpace
         public bool TryLoadLibrary(string file) => LoadLibrary(file, out _);
         public OutputEx LoadLibrary(string file, out OutputEx re, bool ignoreConstructErrors = false, bool ignoreLoadingErrors = true, bool runAssembly = false) {
             re = new OutputEx();
-            _LoadLibrary(file, re, ignoreConstructErrors, ignoreLoadingErrors, runAssembly);
+            try { _LoadLibrary(file, re, ignoreConstructErrors, ignoreLoadingErrors, runAssembly); } catch { }
             return re;
         }
         void _LoadLibrary(string file, OutputEx re, bool ignoreConstructErrors, bool ignoreLoadingErrors, bool runAssembly) {
             if(!File.Exists(file))
             {
-                re.Throw($"Libary file \"{file}\" does not exist!");
-                return;
+                throw re.Throw($"Libary file \"{file}\" does not exist!");
             }
 
             Assembly ass;
             try {
                 ass = Assembly.UnsafeLoadFrom(file);
             } catch (Exception lex) {
-                re.Throw($"Error loading assembly \"{file}\"", lex);
-                return;
+                throw re.Throw($"Error loading assembly \"{file}\"", lex);
             }
 
-            if(!initLibrary(ass, re, ignoreConstructErrors, out var list)) { return; }
-            if(!loadIntefaces(list, re, ignoreLoadingErrors)) { return; }
+            initLibrary(ass, re, ignoreConstructErrors, out var list);
+            loadIntefaces(list, re, ignoreLoadingErrors);
 
-            if(runAssembly) { if(!runStartups(list, re)) { return; } }
+            if(runAssembly) { runStartups(list, re); }
         }
         static bool runStartups(IEnumerable<object> list, OutputEx re) {
             foreach(var o in list) {
                 if(o is IAssemblyStarter st) { st.Start(re); }
             }
-            return re;
+            if (re) { return re; }
+            else { throw re; }
         }
-        static OutputEx createInstances(IEnumerable<Type> types, out IEnumerable<object> list, OutputEx err, bool ignoreConstructErrors) {
+        static void createInstances(IEnumerable<Type> types, out IEnumerable<object> list, OutputEx err, bool ignoreConstructErrors) {
             var re = new List<object>();
+            list = re;
       
             foreach(var o in types) {
                 if(o.IsAbstract || o.IsInterface) { continue; } // do not create abstract classes
@@ -62,27 +62,21 @@ namespace DModulerSpace
                     err.Log($"Cannot create instance of type \"{o.Name}\" !", cex);
                     if(!ignoreConstructErrors) {
                         list = null;
-                        return err.ThrowLast();
+                        throw err.ThrowLast();
                     }
                 }
             }
-
-            list = re;
-            return true;
         }
         static bool isTypeToLoad(Type t) => t.GetInterface(nameof(ILoadable)) != default(Type) || t.GetInterface(nameof(IAssemblyStarter)) != default(Type)|| t.GetInterface(nameof(IAutoshare)) != default(Type);
-        private OutputEx initLibrary(Assembly a, OutputEx err , bool ignoreConstructErrors, out IEnumerable<object> list) {
+        private void initLibrary(Assembly a, OutputEx err , bool ignoreConstructErrors, out IEnumerable<object> list) {
             var types = a.GetTypes();
             // Console.WriteLine($"Types = [{string.Join(" | ", types.ToList())}]");
             var ldbTypes = types.Where(isTypeToLoad);
 
-            if(!createInstances(ldbTypes, out list, err, ignoreConstructErrors)) {
-                return err.Throw($"Cannot init library \"{a.GetName()}\"");
-            }
-
-            return err;
+            try { createInstances(ldbTypes, out list, err, ignoreConstructErrors); }
+            catch { throw err.Throw($"Cannot init library \"{a.GetName()}\""); }
         }
-        private OutputEx loadIntefaces(IEnumerable<object> list, OutputEx err, bool ignoreLoadingErrors) {
+        private void loadIntefaces(IEnumerable<object> list, OutputEx err, bool ignoreLoadingErrors) {
             foreach(var o in list) {
                 if(o is ISharable ish) {
                     sh.AddType(ish.GetType(), ish.Name);
@@ -95,7 +89,7 @@ namespace DModulerSpace
                     try { l.OnAssemblyLoad(this); }
                     catch (Exception lex) {
                         err.Log($"Interface \"{l}\" throwed error on load", lex);
-                        if(!ignoreLoadingErrors) { return err.ThrowLast(); }
+                        if(!ignoreLoadingErrors) { throw err.ThrowLast(); }
                     }
                 }
             }
@@ -104,12 +98,10 @@ namespace DModulerSpace
                     try { l.AfterAssemblyLoad(sh); }
                     catch (Exception aex) {
                         err.Log($"Interface \"{l}\" throwed error after load", aex);
-                        if(!ignoreLoadingErrors) { return err.ThrowLast(); }
+                        if(!ignoreLoadingErrors) { throw err.ThrowLast(); }
                     }
                 }
             }
-
-            return err;
         }
     }
 }
